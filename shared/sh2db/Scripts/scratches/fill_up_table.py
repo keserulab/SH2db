cba7e27e1821b6e6e157bc6992dbfded969f2fb0
takacsagi95 <takacsagi95@gmail.com>
2021-10-17 14:33:52 +0200

    Adding the structures and the scripts which are connected to the sttructure's processing.

--- shared/sh2db/Scripts/scratches/fill_up_table.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-table = pd.read_csv('/home/takacsg/Documents/SH2DB/SH2_domain_containing_prot_new_uniprot_start_stop.csv')
-print(table.head())
-
-table.fillna(method='ffill', inplace = True)
-print(table.head(25))
-
-table.to_csv('/home/takacsg/Documents/SH2DB/SH2_domain_containing_prot_new_uniprot_start_stop_filled.csv')
