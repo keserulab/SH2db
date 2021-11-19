01ba1e6a99b5e9df04d749de78260bd68807bc3a
davidbajusz <bajusz.david@ttk.hu>
2021-11-19 10:28:52 +0100

    Update to views from last week

--- shared/sh2db/home/views.py
+++ shared/sh2db/home/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, 'index.html')
+
+def search(request):
+    return render(request, 'templates/search.html')
+
+def browse(request):
+    return render(request, 'templates/browse.html')
+
+def charts(request):
+    return render(request, 'templates/charts.html')
+
+def faq(request):
+    return render(request, 'templates/faq.html')
+
+def contact(request):
+    return render(request, 'templates/contact.html')
+
--- shared/sh2db/sh2db/urls.py
+++ shared/sh2db/sh2db/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('protein/', include('protein.urls')),
     path('admin/', admin.site.urls),
+    path('', include('home.urls')),
 ]
