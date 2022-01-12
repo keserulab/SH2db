fe44fbc2d72b01e6e571e95ae3ea80f7be517d64
Gáspár Pándy-Szekeres <gaspar@sund.ku.dk>
2022-01-12 21:24:27 +0100

    UPDATE: model update, build script update

---
+++ shared/sh2db/common/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+
+class Publication(models.Model):
+	journal = models.TextField(null=True)
+    title = models.TextField(null=True)
+    authors = models.TextField(null=True)
+    year = models.IntegerField(null=True)
+    reference = models.TextField(null=True)
+
+    def __str__(self):
+    	return '{}-{}-{}...'.format(self.journal, self.year, self.title[:10])
+
+    class Meta():
+        db_table = 'publication'
