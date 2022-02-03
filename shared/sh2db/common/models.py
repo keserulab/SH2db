6c1704d19e46af1fce5c424a5139fa9937a365c7
davidbajusz <bajusz.david@ttk.hu>
2022-02-03 10:36:14 +0100

    minor indent fix in common/models.py

--- shared/sh2db/common/models.py
+++ shared/sh2db/common/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
 
 class Publication(models.Model):
-	journal = models.TextField(null=True)
+    journal = models.TextField(null=True)
     title = models.TextField(null=True)
     authors = models.TextField(null=True)
     year = models.IntegerField(null=True)
