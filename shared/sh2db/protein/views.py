38fbfaa89e29e3b788f41de39dca95a9980eb826
davidbajusz <bajusz.david@ttk.hu>
2022-02-07 16:07:32 +0100

    Browse table linked to database, protein page updated

--- shared/sh2db/protein/views.py
+++ shared/sh2db/protein/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from protein.models import Protein, Domain
+
+
+import requests
+import xml.etree.ElementTree as ET
+
 
 def index(request):
     return HttpResponse("Hello, world. This is SH2db protein page.")
 
 def protein(request, name):
-    return HttpResponse("Hello, world. This is the protein page for %s." % name)
+    protein = Protein.objects.get(name=name)
+    
+    res=requests.get('https://www.uniprot.org/uniprot/'+protein.accession+'.xml')
+    root = ET.fromstring(res.content)
+    entry = root.findall('{http://uniprot.org/uniprot}entry')[0]
+    fullname = entry.findall('{http://uniprot.org/uniprot}protein')[0].findall('{http://uniprot.org/uniprot}recommendedName')[0].findall('{http://uniprot.org/uniprot}fullName')[0].text
+    
+    domains = Domain.objects.filter(isoform_id=protein.id)
+    
+    return render(request, 'protein.html', {'domains' : domains, 'protein': protein, 'fullname': fullname})#, 'headers' : headers})
