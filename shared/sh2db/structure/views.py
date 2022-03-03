e09bbb0fe26d98e78952bab19beeed48066535db
davidbajusz <bajusz.david@ttk.mta.hu>
2022-03-03 18:24:25 +0100

    New templates

--- shared/sh2db/structure/views.py
+++ shared/sh2db/structure/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+from structure.models import Structure, Chain, StructureDomain
+
 def index(request):
     return HttpResponse("Hello, world. This is SH2db structure page.")
 
 def structure(request, pdb_code):
+    try:
+        structure = Structure.objects.get(name=name)
+    except Structure.DoesNotExist:
+        return render(request, 'error.html')
 
-
-
+    return render(request, 'protein.html', {'domains' : domains,  'structuredomains': structuredomains, 'protein': protein, 'fullname': fullname})
     return HttpResponse("Hello, world. This is the protein page for %s." % pdb_code)
     
 def chain(request, pdb_code, chain_ID):
