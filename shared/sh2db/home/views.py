bc76662e09d5342294ebbcd6795b0c370a2967bc
davidbajusz <bajusz.david@ttk.mta.hu>
2022-04-22 16:51:46 +0200

    charts_v0

--- shared/sh2db/home/views.py
+++ shared/sh2db/home/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-from protein.models import Protein, Domain
+from django.http import JsonResponse
 
+from django.db.models import Count
+
+from protein.models import Protein
+
+import pandas as pd
+
+import csv
 
 def index(request):
     return render(request, 'index.html')
@@ -19,8 +26,37 @@ def browse(request):
     
     return render(request, 'browse.html', {'data' : data, 'headers' : headers})
 
+
+def get_csv(request):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(
+        content_type='text/csv',
+        headers={'Content-Disposition': 'attachment; filename="somefilename.csv"'},
+    )
+
+    writer = csv.writer(response)
+    writer.writerow(['group', 'Nitrogen', 'normal', 'stress'])
+    writer.writerow(['banana', '12', '1', '13'])
+    writer.writerow(['poacee', '6', '6', '33'])
+    writer.writerow(['sorgho', '11', '28', '12'])
+    writer.writerow(['triticum', '19', '6', '1'])
+
+    df = pd.DataFrame(columns=['group', 'Nitrogen', 'normal', 'stress'])
+    df.loc[0]=['banana', '12', '1', '13']
+    df.loc[1]=['poacee', '6', '6', '33']
+    df.loc[2]=['sorgho', '11', '28', '12']
+    df.loc[3]=['triticum', '19', '6', '1']
+
+    data = Protein.objects.all().values('family').annotate(count_items=Count('id'))
+
+    return JsonResponse(df.to_json(), safe=False)
+
 def charts(request):
-    return render(request, 'charts.html')
+
+    csv = get_csv(request)
+    #csv = {'group': ['banana', 'poacee', 'sorgho', 'triticum'],'Nitrogen': [12,6,11,19],'normal': [1,6,28,6],'stress': [13,33,12,1]}
+    
+    return render(request, 'charts.html', {'csv' : csv})
 
 def faq(request):
     return render(request, 'faq.html')
