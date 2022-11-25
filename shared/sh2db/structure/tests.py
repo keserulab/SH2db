33bed03d628fe9d1d38d8e98e49d33bab34c2ab7
davidbajusz <bajusz.david@ttk.mta.hu>
2022-11-25 14:16:40 +0100

    updated random stuff

--- shared/sh2db/structure/tests.py
+++ shared/sh2db/structure/tests.py
@@ -1,10 +1,16 @@
+import django
+#django.setup()
+
 from django.test import TestCase
 
 # Create your tests here.
 import sys
+import os
 
 sys.path.append('.')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sh2db.settings")
+django.setup()
 
-import update
+import structure.update
 
-print ( update.to_include(update.pdb_list_from_pdb('P40763'), update.pdb_list_from_sh2db('P40763') ) )
+print ( to_include(pdb_list_from_pdb('P40763'), pdb_list_from_sh2db('P40763') ) )
