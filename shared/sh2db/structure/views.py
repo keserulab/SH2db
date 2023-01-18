188bed511e46d5b5265f255481c315266bf071a2
davidbajusz <bajusz.david@ttk.mta.hu>
2023-01-18 11:33:54 +0100

    minor text edits

--- shared/sh2db/structure/views.py
+++ shared/sh2db/structure/views.py
@@ -36,7 +36,7 @@ def pymoldownload(request):
     io = BytesIO()
 
     ## GENERATE PYMOL SESSION
-    outfilename = 'pymol_session'+str(randint(0,10000000))+'.pse'
+    outfilename = 'SH2db_pymol_session'+str(randint(0,10000000))+'.pse'
     result = subprocess.run(["python2.7", os.getcwd()+"/structure/pymol_session.py", outfilename, 
                                 str([ pdbname for pdbname in pdbnames]), str(residues)], capture_output=True)
 
